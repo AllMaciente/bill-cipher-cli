77a707727bcb7c34d788402db3664250ddee0f86
Allmacdev <allmacdev@gmail.com>
2024-06-05 16:24:16 -0300

    add file use / add possible spaces

--- bill_cipher_cli/main.py
+++ bill_cipher_cli/main.py
@@ -1,32 +1,38 @@
 from bill_cipher_cli import *
 
-from typer import Argument,Option,Typer 
+from typer import Argument, Option, Typer
 
 app = Typer(pretty_exceptions_enable=True)
 
+
 @app.command(help="Encrypt or decrypt a message in Cesar cipher")
 def Cesar(
-    e:bool = Option(False,help="Encypty mode"),
-    d:bool = Option(False,help="Decypt mode"),
-    f:bool = Option(False,help="File encrypt or decrypt"),
-    text:str = Argument(...,help="the msg to encrypt or decrypt"),
-    key:int = Argument(...,help="the key to encrypt or decrypt")
-    ):
+    e: bool = Option(False, help="Encypty mode"),
+    d: bool = Option(False, help="Decypt mode"),
+    file: bool = Option(False, help="encrypt or decrypt one File "),
+    space: bool = Option(False, help="with space (obs Only with --e)"),
+    text: str = Argument(..., help="the msg to encrypt or decrypt"),
+    key: int = Argument(..., help="the key to encrypt or decrypt")
+):
 
     if e or d:
-        if e :
+        if e:
             mode = 'e'
         elif d:
             mode = 'd'
-        print(CesarCipher(text,key,mode))
+        if file:
+            msg = fileRead(text)
+            fileWrite(newFilePath(text, mode),
+                      CesarCipher(msg, key, mode, space))
+        else:
+            print(CesarCipher(text, key, mode, space))
     else:
         print("Information needed flags --e or --d")
 
 
-@app.command()
+@ app.command()
 def Vigenere():
     ...
-
 
 
 app()
