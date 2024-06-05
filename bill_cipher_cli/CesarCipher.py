22b33330e0c93760da27270a3816241da3ae744e
Allmacdev <allmacdev@gmail.com>
2024-06-05 16:22:53 -0300

    add permitir spaces

--- bill_cipher_cli/CesarCipher.py
+++ bill_cipher_cli/CesarCipher.py
@@ -6,12 +6,14 @@ from .utils import alfbet
 """
 
 
-def coretion(text):
+def coretion(text, space: bool = False):
     """
     Converte o texto para minusculo, remove os espaços e troca os ç por c.
 
     Parameters:
         text: o texto a ser convertido.
+        space: se True, mantem os espaços.
+
 
     Examples:
         >>> Coretion('The quick brown fox jumps over the lazy dog')
@@ -19,26 +21,35 @@ def coretion(text):
     """
 
     text = text.lower()
-    text = text.replace(' ', '')
+    if not space:
+        text = text.replace(' ', '')
     text = text.replace('ç', 'c')
     return text
 
 
-def encryption(text, key):
+def encryption(text, key, space: bool = False):
     """
     Encripta o texto usando a chave de encriptação.
 
     Parameters:
         text: o texto a ser encriptado.
         key: a chave de encriptação.
+        space: se True, mantem os espaços.
+
 
     Examples:
         >>> encryption('thequickbrownfoxjumpsoverthelazydog',3)
         'wkhtxlfneurzqiramxpsvryhuwkhodcbgrj'
     """
-    text = coretion(text)
+    text = coretion(text, space)
     textEnctypt = ''
     for letter in text:
+        if letter == ' ':
+            textEnctypt += ' '
+            continue
+        if letter == '\n':
+            textEnctypt += '\n'
+            continue
         ind = alfbet.index(letter)
         ind = (ind + key) % 26
         textEnctypt += alfbet[ind]
@@ -53,20 +64,26 @@ def decryption(text, key):
         text: o texto a ser encriptado.
         key: a chave de encriptação.
 
+
     Examples:
         >>> decryption('wkhtxlfneurzqiramxpsvryhuwkhodcbgrj',3)
         'thequickbrownfoxjumpsoverthelazydog'
     """
-    text = coretion(text)
     textDecypt = ''
     for letter in text:
+        if letter == ' ':
+            textDecypt += ' '
+            continue
+        if letter == '\n':
+            textDecypt += '\n'
+            continue
         ind = alfbet.index(letter)
         ind = (ind - key) % 26
         textDecypt += alfbet[ind]
     return textDecypt
 
 
-def CesarCipher(text, key, mode):
+def CesarCipher(text, key, mode, space: bool = False):
     '''
     Encripta ou decripta o texto usando a chave de encriptação.
 
@@ -84,9 +101,11 @@ def CesarCipher(text, key, mode):
     '''
     match mode:
         case 'e':
-            return encryption(text, key)
+            return encryption(text, key, space)
         case 'd':
             return decryption(text, key)
+        case _:
+            raise ValueError('Mode must be e or d')
 
 
 if __name__ == '__main__':
