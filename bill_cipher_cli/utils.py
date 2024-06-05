3b94266ee8428e8d8e841bf6c6176db24c307e55
Allmacdev <allmacdev@gmail.com>
2024-06-05 15:50:51 -0300

    add file use

--- bill_cipher_cli/utils.py
+++ bill_cipher_cli/utils.py
@@ -1,1 +1,11 @@
 alfbet = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split()
+
+
+def fileRead(filepath):
+    with open(filepath, 'r') as f:
+        return f.read()
+
+
+def fileWrite(filepath, content):
+    with open(filepath, 'w') as f:
+        f.write(content)
