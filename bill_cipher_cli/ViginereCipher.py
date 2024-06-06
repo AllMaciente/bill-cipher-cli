3e778982519ec68e9d9e6ed1a180449dee7cf3fa
Allmacdev <allmacdev@gmail.com>
2024-06-06 16:29:45 -0300

    add: function encrpyt

--- bill_cipher_cli/ViginereCipher.py
+++ bill_cipher_cli/ViginereCipher.py
@@ -24,4 +24,26 @@ def keyMatcheLength(key, text):
     return keyReturn
 
 
-def encrpyt(text, key, space: bool = False): ...
+def encrpyt(text, key, space: bool = False):
+    text = coretion(text, space)
+    key = coretion(key, space)
+    key = keyMatcheLength(key, text)
+    result = ""
+    for n in range(len(text)):
+        if text[n] == "":
+            result += " "
+        elif text[n] == "\n":
+            result += "\n"
+        else:
+            ikey = alfbet.index(key[n])
+            itxt = alfbet.index(text[n])
+            i = (itxt + ikey) % 26
+            result += alfbet[i]
+    return result
+
+
+if __name__ == "__main__":
+    text = "atacar base Sul"
+    key = "LIMAO"
+    cripted = encrpyt(text, key)
+    print(cripted)
